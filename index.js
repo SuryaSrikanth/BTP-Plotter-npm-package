d6198b86c1ba8c26290e02cb86c1d4f245592bae
SuryaSrikanth <srisurya2606@gmail.com>
2020-10-06 11:01:33 +0530

    update 1.0.3

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 import functionPlot from 'function-plot'
 
 function drawFromData(eqn, htmlTag) {
-    // Reading the Equations
+    
     graphTitle = "";
     eqn.equations.forEach(function (funcObj) {
       console.log(funcObj.fn);
       graphTitle = graphTitle.concat(String(funcObj.fn), ", ");
     });
-  
-    // Extra stuff to add if theres time....
-    // Calculate pixels for width and height if given in percent or em.
-    // Path setting size
-    // Setting Background
-    // Append child beside tag
-  
+    
     let JSONdata = {
       title: graphTitle,
       target: htmlTag,
@@ -30,9 +24,9 @@ function drawFromData(eqn, htmlTag) {
       data: eqn.equations,
     };
   
-    // Adding ZoomBox when holding shift
-    JSONdata.plugins = [functionPlot.plugins.zoomBox()];
+    // JSONdata.plugins = [functionPlot.plugins.zoomBox()];
     functionPlot(JSONdata);
+  
   }
 
 module.exports.drawFromData = drawFromData;  
